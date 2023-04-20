87c0440e0da52b0be15070060567ad31b03c1b98
PeptideCate <wldns997@naver.com>
2023-04-20 20:05:28 +0900

    feat : Order test case 작성 완료

--- src/test/java/com/gongbu/ecommerce/order/application/service/BasketServiceTest.java
+++ src/test/java/com/gongbu/ecommerce/order/application/service/BasketServiceTest.java
@@ -1,8 +1,15 @@
 package com.gongbu.ecommerce.order.application.service;
 
+import com.gongbu.ecommerce.item.adapter.out.persistence.ItemJpaEntity;
+import com.gongbu.ecommerce.item.application.port.out.ItemPort;
+import com.gongbu.ecommerce.member.adpater.out.persistence.MemberJpaEntity;
+import com.gongbu.ecommerce.member.application.port.out.AccessMemberPort;
+import com.gongbu.ecommerce.order.adapter.in.web.BasketRequest;
+import com.gongbu.ecommerce.order.adapter.out.persistence.BasketJpaEntity;
 import com.gongbu.ecommerce.order.application.port.out.BasketPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -13,11 +20,42 @@ import static org.hamcrest.CoreMatchers.*;
 @ExtendWith(MockitoExtension.class)
 public class BasketServiceTest {
 
+    @InjectMocks
+    BasketService basketService;
+    @Mock
+    AccessMemberPort accessMemberPort;
+    @Mock
+    ItemPort itemPort;
     @Mock
     BasketPort basketPort;
 
     @Test
     public void addBasketTest() {
+        MemberJpaEntity memberJpaEntity = new MemberJpaEntity();
+        memberJpaEntity.setSeq(1L);
+        memberJpaEntity.setMemberId("test");
 
+        ItemJpaEntity itemJpaEntity = new ItemJpaEntity();
+        itemJpaEntity.setSeq(2L);
+
+        BasketRequest basketRequest = BasketRequest.builder()
+                .memberSeq(1L)
+                .itemSeq(2L)
+                .build();
+
+        BasketJpaEntity basketJpaEntity = new BasketJpaEntity();
+        basketJpaEntity.setSeq(1L);
+        basketJpaEntity.setMemberJpaEntity(memberJpaEntity);
+        basketJpaEntity.setItemJpaEntity(itemJpaEntity);
+
+        when(accessMemberPort.loadMemberJpaEntity(1L)).thenReturn(memberJpaEntity);
+        when(itemPort.loadItemJpaEntity(basketRequest.getItemSeq())).thenReturn(itemJpaEntity);
+
+        BasketJpaEntity findBasketJpaEntity = basketRequest.mapToJpaEntity(memberJpaEntity, itemJpaEntity);
+        when(basketPort.addBasket(findBasketJpaEntity)).thenReturn(1L);
+
+        Long seq = basketService.addBasket(basketRequest);
+
+        assertThat(seq, is(equalTo(1L)));
     }
 }
--- src/test/java/com/gongbu/ecommerce/order/application/service/OrderServiceTest.java
+++ src/test/java/com/gongbu/ecommerce/order/application/service/OrderServiceTest.java
@@ -1,4 +1,60 @@
 package com.gongbu.ecommerce.order.application.service;
+import com.gongbu.ecommerce.item.adapter.out.persistence.ItemJpaEntity;
+import com.gongbu.ecommerce.item.application.port.out.ItemPort;
+import com.gongbu.ecommerce.member.adpater.out.persistence.MemberJpaEntity;
+import com.gongbu.ecommerce.member.application.port.out.AccessMemberPort;
+import com.gongbu.ecommerce.order.adapter.in.web.OrderRequest;
+import com.gongbu.ecommerce.order.adapter.out.persistence.OrderJpaEntity;
+import com.gongbu.ecommerce.order.application.port.out.OrderPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
+
+    @InjectMocks
+    OrderService orderService;
+    @Mock
+    ItemPort itemPort;
+    @Mock
+    AccessMemberPort accessMemberPort;
+    @Mock
+    OrderPort orderPort;
+
+    @Test
+    public void orderTest() {
+        OrderRequest orderRequest = OrderRequest.builder()
+                .memberSeq(1L)
+                .itemSeq(2L)
+                .build();
+
+        ItemJpaEntity itemJpaEntity = new ItemJpaEntity();
+        itemJpaEntity.setSeq(2L);
+
+        MemberJpaEntity memberJpaEntity = new MemberJpaEntity();
+        memberJpaEntity.setSeq(1L);
+        memberJpaEntity.setMyPoint(10L);
+
+        OrderJpaEntity orderJpaEntity = new OrderJpaEntity();
+        orderJpaEntity.setSeq(3L);
+
+        when(itemPort.loadItemJpaEntity(orderRequest.getItemSeq())).thenReturn(itemJpaEntity);
+        when(accessMemberPort.loadMemberJpaEntity(orderRequest.getMemberSeq())).thenReturn(memberJpaEntity);
+
+        OrderJpaEntity findOrderJpaEntity = orderRequest.mapToJpaEntity(memberJpaEntity, itemJpaEntity);
+        when(orderPort.order(findOrderJpaEntity)).thenReturn(3L);
+
+        Long seq = orderService.order(orderRequest);
+
+        assertThat(seq, is(equalTo(3L)));
+    }
+
+
 }
