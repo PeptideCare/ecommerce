764a6dc1e675fb442ef0db3fd4620fae62ff47fd
PeptideCate <wldns997@naver.com>
2023-05-01 19:24:27 +0900

    feat : Board test case 작성완료

--- src/test/java/com/gongbu/ecommerce/board/application/service/BoardServiceTest.java
+++ src/test/java/com/gongbu/ecommerce/board/application/service/BoardServiceTest.java
@@ -4,8 +4,11 @@ import com.gongbu.ecommerce.board.adapter.in.web.BoardRequest;
 import com.gongbu.ecommerce.board.adapter.out.persistence.BoardJpaEntity;
 import com.gongbu.ecommerce.board.adapter.out.persistence.BoardMapper;
 import com.gongbu.ecommerce.board.application.port.out.BoardPort;
+import com.gongbu.ecommerce.board.domain.Board;
+import com.gongbu.ecommerce.board.domain.Comment;
 import com.gongbu.ecommerce.member.adpater.out.persistence.MemberJpaEntity;
 import com.gongbu.ecommerce.member.application.port.out.AccessMemberPort;
+import com.gongbu.ecommerce.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -64,5 +67,66 @@ public class BoardServiceTest {
         String type = boardService.toggleType(1L);
 
         assertThat(type, is(equalTo("secret")));
+    }
+
+    @Test
+    @DisplayName("게시판 불러오기 테스트")
+    public void getBoardTest() {
+        Board board = Board.builder()
+                .seq(new Board.BoardSeq(1L))
+                .title("testTitle")
+                .context("testContext")
+                .memberSeq(new Member.MemberSeq(2L))
+                .build();
+
+        BoardJpaEntity boardJpaEntity = new BoardJpaEntity();
+        boardJpaEntity.setSeq(2L);
+
+        when(boardPort.getBoardJpaEntity(2L)).thenReturn(boardJpaEntity);
+        BoardJpaEntity findBoardJpaEntity = boardPort.getBoardJpaEntity(2L);
+        when(boardMapper.mapToEntity(findBoardJpaEntity)).thenReturn(board);
+        Board findBoard = boardMapper.mapToEntity(findBoardJpaEntity);
+
+        assertThat(findBoard.getSeq().getValue(), is(equalTo(1L)));
+        assertThat(findBoard.getTitle(), is(equalTo("testTitle")));
+        assertThat(findBoard.getContext(), is(equalTo("testContext")));
+    }
+
+    @Test
+    @DisplayName("댓글 쓰기 테스트")
+    public void insertCommentTest() {
+        BoardJpaEntity boardJpaEntity = new BoardJpaEntity();
+        boardJpaEntity.setSeq(1L);
+
+        Comment comment = Comment.builder()
+                .seq(new Comment.CommentSeq(2L))
+                .boardSeq(new Board.BoardSeq(1L))
+                .memberSeq(new Member.MemberSeq(3L))
+                .context("test")
+                .build();
+
+
+        when(boardPort.getBoardJpaEntity(1L)).thenReturn(boardJpaEntity);
+        BoardJpaEntity findBoardJpaEntity = boardPort.getBoardJpaEntity(1L);
+
+        when(boardPort.insertComment(findBoardJpaEntity, comment.getContext())).thenReturn(comment.getSeq().getValue());
+        Long seq = boardPort.insertComment(boardJpaEntity, comment.getContext());
+
+        assertThat(seq, is(equalTo(2L)));
+    }
+
+    @Test
+    @DisplayName("좋아요 테스트")
+    public void addHeartTest() {
+        BoardJpaEntity boardJpaEntity = new BoardJpaEntity();
+        boardJpaEntity.setSeq(1L);
+        boardJpaEntity.setHeart(100L);
+
+        when(boardPort.getBoardJpaEntity(1L)).thenReturn(boardJpaEntity);
+        BoardJpaEntity findBoardJpaEntity = boardPort.getBoardJpaEntity(1L);
+        when(boardPort.addHeart(findBoardJpaEntity)).thenReturn(101L);
+        Long heart = boardPort.addHeart(findBoardJpaEntity);
+
+        assertThat(heart, is(equalTo(101L)));
     }
 }
